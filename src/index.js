d72dbf3b97a829fe6174d9446b4ef07204318e46
killian <killian@studocu.com>
2018-12-04 12:10:37 +0100

    Add redux devtools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,7 +11,7 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers, applyMiddleware(thunk))}>
+  <Provider store={createStoreWithMiddleware(reducers, composeWithDevTools(applyMiddleware(thunk)))}>
     <App />
   </Provider>
   , document.querySelector('.container'));
